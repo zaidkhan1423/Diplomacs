450eec83c7307fd604e8f1705b0a35b4c3e181a3
Zaid khan <Zaidkhan1423@gmail.com>
2023-02-09 01:50:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/diplomacs/SplashScreen.kt
@@ -0,0 +1,27 @@
+package com.example.diplomacs
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashScreen : AppCompatActivity() {
+
+    lateinit var logo: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.screen_splash)
+
+        logo = findViewById(R.id.logo)
+
+        // for intent splash activity to main activity with some transition.
+        logo.alpha = 0f
+        logo.animate().setDuration(2000).alpha(1f).withEndAction {
+            var intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
